87eac6ab983445b775a7af95699eb68039ff6324
Bipika <bpka1990@gmail.com>
2023-09-30 17:14:41 +0300

    code upload

---
+++ precache-manifest.ca79a08204fad97352161864ffda48d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fded19a8d23ac00e7ddc9b56576535a5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5ff131851f8c884372e",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/static/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5ff131851f8c884372e",
+    "url": "/static/js/main.6c83d0aa.chunk.js"
+  },
+  {
+    "revision": "f3260a36ed25041221ee",
+    "url": "/static/js/runtime-main.b584fe3d.js"
+  }
+]);
